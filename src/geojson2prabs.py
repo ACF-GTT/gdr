b5e744437ef34c4ed4804e3195a2d5870e8c8eaf
Alexandre CUER <alexandre.cuer@cerema.fr>
2025-04-23 22:02:42 +0200

    amélioration de la prise en compte des sens

--- src/geojson2prabs.py
+++ src/geojson2prabs.py
@@ -33,57 +33,58 @@ with open(file_name, encoding="utf-8") as geojsonfile:
     tab_pr = []
     features = datas["features"]
     longueur = max(el["properties"]["abs"] for el in features)
-    _STEP = abs(
-        features[1]["properties"]["abs"] - features[0]["properties"]["abs"]
-    )
-    print(f"on a mesuré {longueur} mètres au pas de {_STEP} mètre(s)")
+    # on est forcément dans le sens des abscisses curvilignes croissantes
+    step = features[1]["properties"]["abs"] - features[0]["properties"]["abs"]
+    print(f"on a mesuré {longueur} mètres au pas de {step} mètre(s)")
     for feature in features:
         prop = feature["properties"]
-        coord = feature["geometry"]["coordinates"]
         if prop["pr"] != -1:
             tab_pr.append({
                 "pr": prop["pr"],
                 "abs": prop["abs"],
-                "CFT": prop["CFT"],
-                "lon": coord[0],
-                "lat": coord[1]
+                "CFT": prop["CFT"]
             })
     # est-on dans le sens des pr croissants ?
     SENS = "D" if tab_pr[1]["pr"] - tab_pr[0]["pr"] > 0 else "G"
+    # si on n'est pas dans le sens des pr croissants, on renverse les abscisses curvilignes
     if SENS == "G":
         tab_pr.reverse()
         for _pr in tab_pr:
             _pr["abs"] = longueur - _pr["abs"]
-    print(tab_pr)
-    # on est forcément dans le sens des abscisses curvilignes croissantes
-    # si on n'est pas dans le sens des pr croissants, on renverse les abscisses curvilignes
-    if SENS == "G":
         for feature in features:
             abs_original = feature["properties"]["abs"]
             feature["properties"]["abs"] = longueur - abs_original
         features = reversed(list(features))
+    print(tab_pr)
     INDEX = 0
     NB_PR = len(tab_pr)
-    _CFT = None
-    _PRD = None
-    _ABD = None
     print(f"{NB_PR} points repères sont présents")
     input("appuyer sur une touche pour créer le fichier csv en PR + abscisse")
     csv_name = file_name.replace(".geojson", "_prabs.csv")
     with open(csv_name, 'w', encoding="utf-8") as csv_file:
         writer = csv.writer(csv_file, lineterminator="\n")
         writer.writerow(["CFT", "PRD", "ABD", "PRF", "ABF", "SENS", "DATE"])
+        CFT = None
+        PRD = None
+        ABD = None
         for feature in features:
             prop = feature["properties"]
             if INDEX < NB_PR - 1 and prop["abs"] >= tab_pr[INDEX+1]["abs"]:
                 INDEX += 1
             _prf = tab_pr[INDEX]["pr"]
             _abf = prop["abs"] - tab_pr[INDEX]["abs"]
-            if None not in (_CFT, _PRD, _ABD):
-                writer.writerow([_CFT, _PRD, _ABD, _prf, _abf, SENS, _date])
-            _CFT = prop["CFT"]
-            _PRD = _prf
-            _ABD = _abf
-        _prf = _PRD
-        _abf = _ABD + _STEP
-        writer.writerow([_CFT, _PRD, _ABD, _prf, _abf, SENS, _date])
+            if SENS == "D" and PRD is None:
+                PRD = _prf
+                ABD = _abf - step
+            if SENS == "D":
+                CFT = prop["CFT"]
+            if None not in (CFT, PRD, ABD):
+                writer.writerow([CFT, PRD, ABD, _prf, _abf, SENS, _date])
+            if SENS == "G":
+                CFT = prop["CFT"]
+            PRD = _prf
+            ABD = _abf
+        if SENS == "G":
+            _prf = PRD
+            _abf = ABD + step
+            writer.writerow([CFT, PRD, ABD, _prf, _abf, SENS, _date])
