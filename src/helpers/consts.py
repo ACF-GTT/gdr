310dca2fab1fcac56104a3e4210b0c6370dfe9e9
Alexandre CUER <alexandre.cuer@cerema.fr>
2025-09-09 22:38:10 +0200

    move some consts from generate_si to helpers/consts
    - UPPER, LOWER,
    - LEVELS, LEGENDS,
    - EVE_COLORS, COLORS

--- src/helpers/consts.py
+++ src/helpers/consts.py
@@ -1,4 +1,5 @@
 """constants."""
+from collections import defaultdict
 import logging
 
 FORMAT = "%(asctime)-15s %(levelname)-8s %(module)-15s %(funcName)s :%(lineno)-8s %(message)s"
@@ -9,6 +10,9 @@ LOGGER.setLevel("DEBUG")
 BALISE_RESULTS = "<RESULTS>"
 BALISE_HEADER = "<HEADER>"
 
+############
+# GRIPTESTER
+############
 CFT_POOR = 50
 CFT_GOOD = 60
 CFT_EXCELLENT = 70
@@ -23,6 +27,9 @@ CFT_COLORS = {
 CFT_PENTE = 0.9915
 CFT_DECALAGE = -0.0044
 
+###########
+# RUGOLASER
+###########
 PMP_POOR = 0.4
 PMP_GOOD = 0.8
 
@@ -30,6 +37,82 @@ PMP_COLORS = {
     "poor": "#30383a",
     "fine": "#a2b3a3",
     "good": "#409f48"
+}
+
+COLORS = {
+    "CFT": CFT_COLORS,
+    "PMP": PMP_COLORS
+}
+
+########
+# LEVELS
+########
+UPPER = "upper"
+LOWER = "lower"
+
+LEVELS: dict[str, dict[str, dict[str, int | float]]] = {
+    "CFT": {
+        "poor": {UPPER: CFT_POOR},
+        "fine": {LOWER: CFT_POOR, UPPER: CFT_GOOD},
+        "good": {LOWER: CFT_GOOD, UPPER: CFT_EXCELLENT},
+        "excellent": {LOWER: CFT_EXCELLENT}
+    },
+    "PMP": {
+        "poor": {UPPER: PMP_POOR},
+        "fine": {LOWER: PMP_POOR, UPPER: PMP_GOOD},
+        "good": {LOWER: PMP_GOOD}
+    }
+}
+
+#####################
+# LEGENDS (AUTOMATIC)
+#####################
+LEGENDS: dict[str, dict[str, str]] = {}
+
+for metric, levels_description in LEVELS.items():
+    if metric not in LEGENDS:
+        LEGENDS[metric] = {}
+    for level, bounds in levels_description.items():
+        lower = bounds.get(LOWER)
+        upper = bounds.get(UPPER)
+        if lower is None and upper is not None:
+            LEGENDS[metric][level] = f"{metric}<={upper}"
+            continue
+        if lower is not None and upper is None:
+            LEGENDS[metric][level] = f"{metric}>{lower}"
+            continue
+        LEGENDS[metric][level] = f"{lower}<{metric}<={upper}"
+
+#############
+# MANUAL TOPS
+#############
+EVE_STD = {
+    "font": "red",
+    "line": "blue",
+    "bbox": {
+        "boxstyle": "round",
+        "edgecolor": "gray",
+        "facecolor": "white"
+    }
+}
+EVE_COLORS: dict[str, dict] = defaultdict(lambda: EVE_STD)
+EVE_COLORS["D"] = {
+    "font": "white",
+    "line": "green",
+    "bbox": {
+        "boxstyle": "round",
+        "edgecolor": "none",
+        "facecolor": "green"
+    }
+}
+EVE_COLORS["F"] = {
+    "font": "white",
+    "line": "red",
+    "bbox": {
+        "boxstyle": "round",
+        "edgecolor": "none",
+        "facecolor": "red"
+    }
 }
 
 def correle(gt_value):
