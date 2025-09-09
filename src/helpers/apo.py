273cfc16f8477425ac36e218e0163896d1e182e9
Alexandre CUER <alexandre.cuer@cerema.fr>
2025-09-09 21:35:05 +0200

    Apo - round top abs to measuring step

--- src/helpers/apo.py
+++ src/helpers/apo.py
@@ -16,12 +16,6 @@ def get_apo_datas(
     folder = os.path.dirname(file_name)
     all_files = os.listdir(folder)
     eve_name = [el for el in all_files if el.endswith(".EV0")][0]
-    with open(f"{folder}/{eve_name}", encoding="utf-8") as evefile:
-        for i,row in enumerate(
-            csv.reader(evefile, delimiter='\t')
-        ):
-            if row[0].lower() == PR:
-                tops[row[2]] = (float(row[1]), 0.0)
     with open(file_name, encoding="utf-8") as datafile:
         data = csv.reader(datafile, delimiter='\t')
         unit_index = None
@@ -33,6 +27,12 @@ def get_apo_datas(
                     step = float(row[1]) - float(row[0])
                 y_datas.append(float(row[unit_index]))
     if step is not None:
+        with open(f"{folder}/{eve_name}", encoding="utf-8") as evefile:
+            for i,row in enumerate(
+                csv.reader(evefile, delimiter='\t')
+            ):
+                if row[0].lower() == PR:
+                    tops[row[2]] = (round(float(row[1])/step)*step, 0.0)
         # pylint: disable=duplicate-code
         return RoadMeasure(
             step=step,
