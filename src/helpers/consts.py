d7b23b6265febeca00e985c82495a5c1d75b4435
huugoperez <hugo-h.perez@cerema.fr>
2025-10-01 11:16:36 +0200

    const modifié

--- src/helpers/consts.py
+++ src/helpers/consts.py
@@ -29,16 +29,21 @@ PR = "pr"
 CFT_POOR = 50
 CFT_GOOD = 60
 CFT_EXCELLENT = 70
+CFL_POOR = 50
+CFL_GOOD = 60
+CFL_EXCELLENT = 70
 CFT_COLORS = {
     POOR: "#ea1256",
     FINE: "#ee7a0e",
     GOOD: "#e7dc0c",
     EXCELLENT: "#38e70c"
 }
-
-# droite de corrélation obtenue lors des essais croisés
-CFT_PENTE = 0.9915
-CFT_DECALAGE = -0.0044
+CFL_COLORS = {
+    POOR: "#ea1256",
+    FINE: "#ee7a0e",
+    GOOD: "#e7dc0c",
+    EXCELLENT: "#38e70c"
+}
 
 ###########
 # RUGOLASER
@@ -54,8 +59,9 @@ PMP_COLORS = {
 
 UNKNOWN_COLOR = "#cccccc"
 COLORS = {
-    "CFT": CFT_COLORS,
-    "PMP": PMP_COLORS
+    "CFL": CFL_COLORS, #grip
+    "PMP": PMP_COLORS, #rugolaser
+    "CFT": CFT_COLORS  #scrim avec le même seuil que CFL car CFT ~ CFL
 }
 
 ########
@@ -65,16 +71,22 @@ UPPER = "upper"
 LOWER = "lower"
 
 LEVELS: dict[str, dict[str, dict[str, int | float]]] = {
-    "CFT": {
-        POOR: {UPPER: CFT_POOR},
-        FINE: {LOWER: CFT_POOR, UPPER: CFT_GOOD},
-        GOOD: {LOWER: CFT_GOOD, UPPER: CFT_EXCELLENT},
-        EXCELLENT: {LOWER: CFT_EXCELLENT}
+    "CFL": {
+        POOR: {UPPER: CFL_POOR},
+        FINE: {LOWER: CFL_POOR, UPPER: CFL_GOOD},
+        GOOD: {LOWER: CFL_GOOD, UPPER: CFL_EXCELLENT},
+        EXCELLENT: {LOWER: CFL_EXCELLENT}
     },
     "PMP": {
         POOR: {UPPER: PMP_POOR},
         FINE: {LOWER: PMP_POOR, UPPER: PMP_GOOD},
         GOOD: {LOWER: PMP_GOOD}
+    },
+    "CFT": {
+        POOR: {UPPER: CFT_POOR},
+        FINE: {LOWER: CFT_POOR, UPPER: CFT_GOOD},
+        GOOD: {LOWER: CFT_GOOD, UPPER: CFT_EXCELLENT},
+        EXCELLENT: {LOWER: CFT_EXCELLENT}
     }
 }
 
@@ -133,9 +145,6 @@ EVE_COLORS["F"] = {
     }
 }
 
-def correle(gt_value):
-    """Applique la corrélation issue des essais croisés."""
-    return 100*(CFT_PENTE * gt_value + CFT_DECALAGE)
 
 def get_color(val: float, unit: str = "CFT") -> str:
     """Couleur du couple (valeur, type de mesure)."""
